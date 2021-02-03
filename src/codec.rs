8834228f04c652e97f20717606a7dd2c3cdc108e
berry3 <berry3@indiana.edu>
2021-02-03 17:54:42 -0500

    updating codecs to use LigatureError and adding functions for new types, #7

--- src/codec.rs
+++ src/codec.rs
@@ -7,21 +7,83 @@ use ligature::{
     Vertex, WriteTx,
 };
 
-use bincode::Error;
-
 const DATASET_PREFIX: u8 = 0;
 
-pub fn encode_dataset(dataset: &Dataset) -> Result<Vec<u8>, Error> {
-    let encoded_dataset = bincode::serialize(dataset.name())?;
+pub fn encode_dataset(dataset: &Dataset) -> Result<Vec<u8>, LigatureError> {
+    let encoded_dataset = bincode::serialize(dataset.name())
+        .map_err(|_| LigatureError("Error checking for Dataset".to_string()))?;
     let mut res = vec![DATASET_PREFIX];
     res.extend(&encoded_dataset);
     Ok(res)
 }
 
-pub fn decode_dataset(dataset: Vec<u8>) -> Result<Dataset, Error> {
+pub fn decode_dataset(dataset: Vec<u8>) -> Result<Dataset, LigatureError> {
     let mut dataset_clone = dataset.clone();
     dataset_clone.remove(0);
     bincode::deserialize(&dataset_clone)
+        .map_err(|_| LigatureError("Error checking for Dataset".to_string()))
+}
+
+pub fn encode_vertex(vertex: &Vertex) -> Vec<u8> {
+    match vertex {
+        Vertex::Node(node) => encode_node(node),
+        Vertex::StringLiteral(value) => encode_string(value),
+        Vertex::BooleanLiteral(value) => encode_boolean(value),
+        Vertex::LongLiteral(value) => encode_long(value),
+        Vertex::FloatLiteral(value) => encode_float(value),
+    }
+}
+
+pub fn decode_vertex(vertex: Vec<u8>) -> Vertex {
+    todo!()
+}
+
+pub fn encode_node(node: &Node) -> Vec<u8> {
+    todo!()
+}
+
+pub fn decode_node(node: Vec<u8>) -> Node {
+    todo!()
+}
+
+pub fn encode_string(string: &String) -> Vec<u8> {
+    todo!()
+}
+
+pub fn decode_string(string: Vec<u8>) -> String {
+    todo!()
+}
+
+pub fn encode_boolean(boolean: &bool) -> Vec<u8> {
+    todo!()
+}
+
+pub fn decode_boolean(node: Vec<u8>) -> bool {
+    todo!()
+}
+
+pub fn encode_long(long: &i64) -> Vec<u8> {
+    todo!()
+}
+
+pub fn decode_long(long: Vec<u8>) -> i64 {
+    todo!()
+}
+
+pub fn encode_float(float: &f64) -> Vec<u8> {
+    todo!()
+}
+
+pub fn decode_float(node: Vec<u8>) -> f64 {
+    todo!()
+}
+
+pub fn encode_arrow(arrow: &Arrow) -> Vec<u8> {
+    todo!()
+}
+
+pub fn decode_arrow(arrow: Vec<u8>) -> Arrow {
+    todo!()
 }
 
 pub fn encode_link(link: &Link) -> Vec<u8> {
@@ -37,29 +99,5 @@ pub fn encode_persisted_link(persisted_link: &PersistedLink) -> Vec<u8> {
 }
 
 pub fn decode_persisted_link(persisted_link: Vec<u8>) -> PersistedLink {
-    todo!()
-}
-
-pub fn encode_vertex(vertex: &Vertex) -> Vec<u8> {
-    todo!()
-}
-
-pub fn decode_vertex(vertex: Vec<u8>) -> Vertex {
-    todo!()
-}
-
-pub fn encode_arrow(arrow: &Arrow) -> Vec<u8> {
-    todo!()
-}
-
-pub fn decode_arrow(arrow: Vec<u8>) -> Arrow {
-    todo!()
-}
-
-pub fn encode_node(node: &Node) -> Vec<u8> {
-    todo!()
-}
-
-pub fn decode_node(node: Vec<u8>) -> Node {
     todo!()
 }
